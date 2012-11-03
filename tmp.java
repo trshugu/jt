94673d39678e23b77b0196767c81699d26e3db3b
trshuguntu <trshugu@gmial.com>
2012-11-04 01:06:02 +0900

    20121104 基本的なあたりから・・

--- tmp.java
+++ tmp.java
@@ -3,7 +3,48 @@ public static void main(String args[]){
 /* 
 */
 
+// 配列
+String[] strarr = null;
+//strarr.add("anoano");
 
+/* 
+// 参照型の確認
+StringBuilder sb = new StringBuilder().append("anokutara");
+StringBuilder ref = new StringBuilder();
+ref = sb;
+
+System.out.println(sb);
+System.out.println(ref);
+
+ref.append("tuika");
+System.out.println(sb);
+System.out.println(ref);
+*/
+
+
+/* 
+// StringBufferとStringBuilder
+StringBuilder sb = new StringBuilder();
+sb.append("anokutara");
+sb.append("sanmyaku");
+System.out.println(sb);
+
+StringBuffer sbuf = new StringBuffer();
+sbuf.append("sanbodai");
+sbuf.append("noumaku");
+System.out.println(sbuf.toString());
+*/
+
+
+/* 
+// primitive? or reference? 2
+String stst = new String("asfdsdf");
+String atoato = new String();
+atoato = stst;
+atoato = "henkou!";
+System.out.println(stst);
+System.out.println(atoato);
+*/
 
 
 /* 
