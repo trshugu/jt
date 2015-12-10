74f15ae843b10061b092b9a69ce47fd3e5ee19e9
anotkuta <anotkuta@samnm.yalk>
2015-12-10 11:46:03 +0900

    gitpush2015/12/10

--- tmp.java
+++ tmp.java
@@ -20,9 +20,11 @@ public class tmp {
   
   public static void main(String args[]) {
     File files[] = listFiles("./","*");
+    /*
     for (int i=0; i<files.length; i++) {
         System.out.println("ファイル" + (i+1) + "→" + files[i]);
     }
+    */
   }
 
   public static File[] listFiles(String directoryPath, String fileName) {
@@ -42,6 +44,9 @@ public class tmp {
       ("引数で指定されたパス[" + dir.getAbsolutePath() + "]はディレクトリではありません。");
     }
     File[] files = dir.listFiles();
+    for (int i=0; i<files.length; i++) {
+        System.out.println("ファイル" + (i+1) + "→" + files[i]);
+    }
     
     // その出力
     for (int i = 0; i < files.length; i++) {
