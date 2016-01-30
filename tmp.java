3585845450fb75225b17e2bfa179783e7117a3d0
anokutara <anokutara@sanmyaku.com>
2016-01-30 18:28:50 +0900

    gitpush2016/01/30

--- tmp.java
+++ tmp.java
@@ -8,6 +8,49 @@ public class tmp {public static void main(String args[]) {
 
 
 /*
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+class tmp
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, String> map = new HashMap<>();
+        try {
+            while (true) {
+                String line = br.readLine();
+                if (line == null || line.length() == 0) {
+                    break;
+                }
+                String[] keyValues = line.split(",");
+                map.put(keyValues[0], keyValues[1]);
+            }
+        } finally {
+            br.close();
+        }
+        if (map.containsKey("java")){
+          System.out.println(map.get("java").toUpperCase());
+        } else{
+          System.out.println();
+        }
+        if (map.containsKey("ruby")){
+          System.out.println(map.get("ruby").toUpperCase());
+        } else{
+          System.out.println();
+        }
+        if (map.containsKey("go")){
+          System.out.println(map.get("go").toUpperCase());
+        } else{
+          System.out.println();
+        }
+    }
+}
+*/
+
+
+
+/*
 import java.math.*;
 class tmp{
 public static void main(String[] args){
